08a6c511e32cce1b250cf62d90dc49717945a092
Dianajarenga <jarengadiana@gmail.com>
2021-02-21 12:26:33 +0300

    first commit

--- src/Hello.kt
+++ src/Hello.kt
@@ -1,9 +1,28 @@
-import javax.print.attribute.standard.MediaName
 
-fun main(){
-    val num = arrayListOf (10,11)
-    println(num)
-    val name : String ="Dina"
-    print(name)
+
+
+fun main() {
+    println("STUDENT RECORDS")
+    val name = "Diana Jarenga"
+    println(name)
+    val Age = "Age :  "
+    print(Age)
+    var Int = 20
+    Int = 21
+    println(Int)
+    val phoneNumber = "Phone Number"
+    print(phoneNumber)
+    var number = " : +254 713 535 7064"
+    println(number)
+    var weight = "Weight  :"
+    print(weight)
+    var int = " 56"
+    print(int)
+    val nam = " Kg"
+    println(nam)
+    var Citizenship="Citizenship : kenyan"
+    println(Citizenship)
+    val kenyanCitizenship:Boolean= true
+    println(kenyanCitizenship)
 
 }
